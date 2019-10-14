db72b73244a3a6a1998e78a99a00ee98cb186abe
lizhipeng <phcj1235@163.com>
2019-10-14 09:59:49 +0800

    First push

---
+++ Test2/Models/CostItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test2.Models
+{
+    public class CostItem
+    {
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+        public string About { get; set; }
+    }
+}
